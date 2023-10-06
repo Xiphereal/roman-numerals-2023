9b2e33035c92ed67b6edfdf3505e5b75d3fc1407
Xiphereal <lozanojalex@gmail.com>
2023-10-06 19:14:13 +0200

    red: Convert1toI

---
+++ java/src/main/java/Arabic.java
@@ -0,0 +1,5 @@
+public class Arabic {
+  public static String toRoman(int i) {
+    return "I";
+  }
+}
--- java/src/test/java/ClassTest.java
+++ java/src/test/java/ClassTest.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ClassTest {
 
 	@Test
-	public void renameMe() {
-		assertTrue(true);
+	public void Convert1toI() {
+		assertEquals("I", Arabic.toRoman(1));
 	}
 
 }
